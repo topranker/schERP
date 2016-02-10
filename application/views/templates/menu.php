50f603a825f2e9419d8a7fd83165c13563f63595
Nitin Deepak <nitin.d12@gmail.com>
2016-02-10 22:06:43 +0530

    updates
    updates

--- application/views/templates/menu.php
+++ application/views/templates/menu.php
@@ -2,23 +2,25 @@
           <div id="sidebar"  class="nav-collapse ">
               <!-- sidebar menu start-->
               <ul class="sidebar-menu">                
-                <li class="active">
-                  <a class="" href="<?PHP echo site_url('web');?>">
-                      <i class="icon_house_alt"></i>
-                      <span>Dashboard</span>
-                  </a>
-                </li>
+                
+                <?php foreach($menu_ as $item){ ?>
                 <li class="sub-menu">
-                      <a href="javascript:;" class="">
-                          <i class="fa fa-users"></i>
-                          <span>Registration</span>
-                          <span class="menu-arrow arrow_carrot-right"></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a class="" href="<?PHP echo site_url('web/stuRegistration');?>"><i class="fa fa-child"></i> New Registration</a></li>                          
-                          <li><a class="" href="<?PHP echo site_url('web/editReg');?>"><i class="fa fa-pencil-square-o"></i> Edit Registration</a></li>
-                      </ul>
-                    </li> 
+                  <a href="<?php if($item -> PATH_ == 'x') { echo "javascript:;"; } else { echo site_url($item -> PATH_); } ?>" class="">
+                      <i class="<?php echo $item -> PRE_ICON ;?>"></i>
+                      <span><?php echo $item -> MENU ; ?></span>
+                      <?php if($item -> PATH_ == 'x') { ?><span class="menu-arrow arrow_carrot-right"></span><?php } ?>
+                  </a>
+                  <?php //if(in_array($item -> ID_, $submenu_, TRUE)) { ?>
+                  <ul class="sub">
+                      <?php foreach($submenu_ as $subitem) { ?>
+                        <?php if($subitem -> ID_ == $item -> ID_) { ?>
+                          <li><a class="" href="<?PHP echo site_url($subitem -> PATH_);?>"><i class="<?php echo $subitem -> PRE_ICON; ?>"></i><?php echo $subitem -> SUBMENU; ?></a></li>                          
+                        <?php } ?>
+                      <?php } ?>
+                  </ul>
+                  <?php //} ?>
+                </li>
+                <?php } ?>
               </ul>
               <!-- sidebar menu end-->
           </div>
